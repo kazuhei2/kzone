bfcd9a40829e62a89eb19c29e80c4b241dc4512e
Kazuhei Hamada <kazuhei.git@gmail.com>
2020-07-06 22:59:35 +0900

    Change Home and router

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
 <template>
   <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+    >
+    <v-container>
+      <v-list dense nav>
+        <v-list-item
+          v-for="item in navList"
+          :key="item.name"
+          :to="item.link"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.name }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-container>
+    </v-navigation-drawer>
+
     <v-app-bar
       app
-      color="primary"
-      dark
+      clipped-left
     >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -14,47 +37,47 @@
           transition="scale-transition"
           width="40"
         />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
       </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-toolbar-title>kzone</v-toolbar-title>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <v-container>
+        <router-view/>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
   },
 
   data: () => ({
-    //
+    drawer: null,
+    navList: [
+      {
+        name: 'Home',
+        icon: 'mdi-home',
+        link: '/'
+      },
+      {
+        name: 'About',
+        icon: 'mdi-information',
+        link: '/about'
+      },
+      {
+        name: 'DevelopersRoom',
+        icon: 'mdi-android-debug-bridge',
+        link: '/developers-room'
+      },
+    ],
   }),
+
+  methods: {
+  },
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -13,11 +13,13 @@ Vue.use(VueRouter)
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: () => import('../views/About.vue')
+  },
+  {
+    path: '/developers-room',
+    name: 'DevelopersRoom',
+    component: () => import('../views/DevelopersRoom.vue')
+  },
 ]
 
 const router = new VueRouter({
